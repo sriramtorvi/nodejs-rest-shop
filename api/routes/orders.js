71361aaa3e8a4d350ab7520f39ddd3934e6045a3
sriramtorvi <sriram.torvi@gmail.com>
2019-12-18 22:19:31 +0530

    first commit

---
+++ api/routes/orders.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Order = require('../models/order');
+const Product = require('../models/product');
+
+router.get('/', (req, res, next) => {
+    Order.find()
+        .select('product quantity _id')
+        .populate('product', 'name')
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                count: docs.length,
+                orders: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/orders/' + doc._id
+                        }
+                    }
+                })
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
+});
+
+router.post('/', (req, res, next) => {
+    Product.findById(req.body.productId)
+        .then(product => {
+            if(!product) {
+                return res.status(404).json({
+                    message: 'Product not found'
+                });
+                
+            }
+            const order = new Order({
+                _id: new mongoose.Types.ObjectId(),
+                product: req.body.productId,
+                quantity: req.body.quantity
+            });
+            return order.save();        
+        })
+        .then(result => {
+            if(res.statusCode === 404){
+                return res;
+            }
+            console.log(result);
+            res.status(201).json({
+                message: 'Order created',
+                order: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders/' + result._id
+                }
+
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+    
+});
+
+router.get('/:orderId',(req, res, next) => {
+    Order.findById(req.params.orderId)
+        .populate('product')
+        .exec()
+        .then(order => {
+            if(order){
+                res.status(200).json({
+                    order: {
+                        _id: order._id,
+                        product: order.product,
+                        quantity: order.quantity
+                    },
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders'
+                    }
+                });               
+            } else {
+                res.status(404).json({
+                    message: 'Order not found'
+                });
+            }
+            
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        })
+});
+
+router.delete('/:orderId',(req, res, next) => {
+    Order.remove({_id: req.params.orderId})
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'Order deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/orders',
+                    body: {
+                        product: 'ID',
+                        quantity: 'Number'
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
+});
+
+module.exports = router;
---
+++ api/routes/products.js
@@ -0,0 +1,141 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const Product = require('../models/product')
+
+router.get('/', (req, res, next) => {
+    Product.find()
+        .select('name price _id')
+        .exec()
+        .then(results => {
+            const response = {
+                count: results.length,
+                product: results.map(result => {
+                    return {
+                        name: result.name,
+                        price: result.price,
+                        _id: result._id,
+                        request: {
+                            type: 'GET',
+                            url: 'http://localhost:3000/products/' + result._id
+                        }
+                    }
+                })
+            };
+            res.status(200).json(response);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
+});
+
+router.post('/', (req, res, next) => {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
+        name: req.body.name,
+        price: req.body.price
+    });
+    product.save().then(result => {
+        console.log(result);
+        res.status(200).json({
+            createdProduct: {
+                name: result.name,
+                price: result.price,
+                _id: result._id,
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products/' + result._id
+                }
+            }
+        });
+    })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.get('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.findById(id)
+        .select('name price _id')
+        .exec()
+        .then(result => {
+            console.log(result);
+            if (result) {
+                res.status(200).json({
+                    product: result,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/products'
+                    }
+                });
+            } else {
+                res.status(404).json({ message: 'No data found' });
+            }
+
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({ error: err });
+        });
+});
+
+router.patch('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    const updateOps = {};
+    for (const ops of req.body) {
+        updateOps[ops.propName] = ops.value;
+    }
+    Product.update({ _id: id }, { $set: updateOps }).exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Product updated',
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/products' + id
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+
+router.delete('/:productId', (req, res, next) => {
+    const id = req.params.productId;
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'Product deleted',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/products',
+                    body: {
+                        name: 'String',
+                        price: 'Number'
+                    }
+                }
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
+});
+
+module.exports = router;
